7b0e58f686354cd04fda39c6f9371654af7d5a8b
rkuczynski1531 <62150104+rkuczynski1531@users.noreply.github.com>
2022-06-29 16:15:20 +0200

    Delete rekrutacja directory

--- rekrutacja/src/main/java/com/example/rekrutacja/repository/ResearchOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rekrutacja.repository;
-
-import com.example.rekrutacja.model.ResearchOrder;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ResearchOrderRepository extends JpaRepository<ResearchOrder, Long> {
-}
